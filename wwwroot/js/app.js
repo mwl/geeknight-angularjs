9c5dd2383fa996ee3c514305e81b8c7ae53e8ca9
Martin Westergaard Lassen <martin@mwl.dk>
2013-03-02 19:50:58 +0100

    Starting point

--- wwwroot/js/app.js
+++ wwwroot/js/app.js
@@ -1,13 +1,1 @@
-var module = angular.module('todo', []);
-module.config(function ($routeProvider) {
-    $routeProvider.
-        when('/', {controller: HomeCtrl, templateUrl: 'home.html'}).
-        when('/lists/:listname/', {controller: ListCtrl, templateUrl: 'list.html'}).
-        otherwise({redirectTo: '/'});
-});
-function HomeCtrl($scope) {
-}
-
-function ListCtrl($scop) {
-
-}
+'use strict';

c4e2b17176d6792bd3edef7ddddecd6661c0b997
Martin Westergaard Lassen <martin@mwl.dk>
2013-03-02 21:26:09 +0100

    Displaying a list of tasks

--- wwwroot/js/app.js
+++ wwwroot/js/app.js
@@ -1,1 +1,11 @@
 'use strict';
+
+var module = angular.module('todo', []);
+
+function ListsCtrl($scope) {
+    $scope.lists = [
+        {name: "Angular geeknight"},
+        {name: "Personlige opgaver"}
+    ]
+
+}

4813c375c9f3f9f45e25baa12fec07a9dbe225bb
Martin Westergaard Lassen <martin@mwl.dk>
2013-03-03 15:46:18 +0100

    Loading lists from backend

--- wwwroot/js/app.js
+++ wwwroot/js/app.js
@@ -2,10 +2,15 @@
 
 var module = angular.module('todo', []);
 
-function ListsCtrl($scope) {
-    $scope.lists = [
-        {name: "Angular geeknight"},
-        {name: "Personlige opgaver"}
-    ]
+function ListsCtrl($scope, $http) {
+    $scope.lists = []
 
+    $http({method: 'GET', url: '/buckets/lists/keys/mwl', cache: false}).
+        success(function (data, status, headers, config) {
+            $scope.lists = data.lists;
+        }).
+        error(function (data, status, headers, config) {
+
+        }
+    );
 }

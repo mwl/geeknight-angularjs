2c9eacbb6506b0a3b0bceaa7cca5a9a35a285e1e
Martin Westergaard Lassen <mwl@trifork.com>
2013-03-05 14:36:48 +0100

    Demo 3 with data from riak through ngResource

--- wwwroot/demo3/app.js
+++ wwwroot/demo3/app.js
@@ -1,29 +1,31 @@
 var app = angular.module('Demo3', ['ngResource']);
 app.config(function ($routeProvider, $locationProvider) {
     $routeProvider.
-        when("/", {controller: SeasonCtrl, templateUrl: "/demo3/season.html"}).
+        when("/:year", {controller: SeasonCtrl, templateUrl: "/demo3/season.html"}).
         when("/circuits/:name", {controller: CircuitCtrl, templateUrl: "/demo3/circuit.html"}).
-        otherwise({redirectTo: '/'});
-    //$locationProvider.html5Mode(true);
+        otherwise({redirectTo: '/2013'});
+});
+app.factory('Season', function ($resource) {
+    return $resource("/buckets/seasons/keys/:year", {year: '@year'})
 });
 app.factory('Circuit', function($resource) {
-    return $resource('/buckets/circuits/keys/:name', {name: '@handle'})
+    return $resource('/buckets/circuits/keys/:name', {name: '@id'})
 });
 
 
-
-function SeasonCtrl($scope, $routeParams) {
-    $scope.year = $routeParams.year;
-    $scope.circuits = [
-        {handle: "spa", name: "Spa-Francorchamps"}
-    ]
+function SeasonCtrl($scope, $routeParams, Season) {
+    $scope.season = Season.get({year: $routeParams.year})
 }
 
 function CircuitCtrl($scope, $routeParams, Circuit) {
 
-    $scope.circuit = Circuit.get({name: 'spa'});
+    $scope.circuit = Circuit.get({name: $routeParams.name});
 
     $scope.hasSpeed = function(corner) {
         return corner.speed > 0;
     }
+
+    $scope.save = function() {
+        $scope.circuit.$save();
+    };
 }

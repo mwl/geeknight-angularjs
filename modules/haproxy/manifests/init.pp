bd70a3e26e26a2eace5945859a2d1ff779e29586
Martin Westergaard Lassen <martin@mwl.dk>
2014-07-23 18:17:04 +0100

    Modernized modules

--- modules/haproxy/manifests/init.pp
+++ modules/haproxy/manifests/init.pp
@@ -2,29 +2,18 @@ class haproxy() {
 	package {
 		"haproxy":
 			ensure => installed,
-	}
-
+	} ->
 	file {
 		"/etc/default/haproxy":
 			ensure => present,
-			require => Package["haproxy"],
-			content => "ENABLED=1"
-	}
-
-	file {
+			content => "ENABLED=1";
 		"/etc/haproxy/haproxy.cfg":
 			ensure => present,
-			source => "puppet:///modules/haproxy/haproxy.cfg",
-			require => Package["haproxy"],
-			notify => Service["haproxy"]
-	}
-
+			source => "puppet:///modules/haproxy/haproxy.cfg";
+	} ~>
 	service {
 		"haproxy":
 			ensure => running,
-			require => [Package["haproxy"], File["/etc/default/haproxy"]],
-			hasrestart => true,
-			hasstatus => true
 	}
 			
 }
--- modules/lighttpd/manifests/init.pp
+++ modules/lighttpd/manifests/init.pp
@@ -1,24 +1,18 @@
 class lighttpd() {
-    file {"/etc/lighttpd":
-        ensure => directory,
-        owner => root,
-        group => root
-    }
-    file {"/etc/lighttpd/lighttpd.conf":
-        ensure => present,
-        require => File["/etc/lighttpd"],
-        source => "puppet:///modules/lighttpd/lighttpd.conf"
-    }
-
-    package {"lighttpd":
-        ensure => installed,
-        require => File["/etc/lighttpd/lighttpd.conf"]
-    }
-
-    service {"lighttpd":
-        ensure => running,
-        require => Package["lighttpd"],
-        hasstatus => true,
-        hasrestart => true,
+    package {
+        "lighttpd":
+            ensure => installed,
+            require => File["/etc/lighttpd/lighttpd.conf"];
+    } ->
+    file {
+        "/etc/lighttpd":
+            ensure => directory;
+        "/etc/lighttpd/lighttpd.conf":
+           ensure => present,
+            source => "puppet:///modules/lighttpd/lighttpd.conf";
+    } ~>
+    service {
+        "lighttpd":
+            ensure => running;
     }
 }
